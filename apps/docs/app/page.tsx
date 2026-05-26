d0fa9ba24d97adf33222237a45139d5cf2de3120
Simon Steer <simonpsteer@gmail.com>
2026-05-26 09:55:36 -0400

    rename style.css -> styles.css

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -1,16 +1,14 @@
 import { Icon } from "@repo/react/Icon";
 
-import "@repo/tokens/style.css";
+import "@repo/tokens/styles.css";
 
 export default function Page() {
   return (
-    <div style={{ colorScheme: "dark" }}>
-      <Icon
-        kind="tenstorrent"
-        style={{ color: "var(--stone-500)" }}
-        width={48}
-        height={48}
-      />
-    </div>
+    <Icon
+      kind="tenstorrent"
+      style={{ color: "var(--teal-500)" }}
+      width={48}
+      height={48}
+    />
   );
 }
