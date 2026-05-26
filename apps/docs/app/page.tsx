80096208c473cab3dd408850bbd734574ec6defc
Simon Steer <simonpsteer@gmail.com>
2026-05-26 14:39:07 -0400

    Update page.tsx

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -1,11 +1,5 @@
 import { Tenstorrent } from "@repo/vesper/icons";
 
 export default function Page() {
-  return (
-    <Tenstorrent
-      style={{ color: "var(--vesper-teal-500)" }}
-      width={48}
-      height={48}
-    />
-  );
+  return <Tenstorrent color="var(--vesper-teal-500)" width={48} height={48} />;
 }
