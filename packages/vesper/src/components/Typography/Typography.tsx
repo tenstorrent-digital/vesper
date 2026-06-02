662985e87be1930b3bb63f5d8e9b2008e532bbb4
Simon Steer <simonpsteer@gmail.com>
2026-06-02 14:25:24 -0400

    first pass at Typography component

--- packages/vesper/src/components/Typography/Typography.tsx
+++ packages/vesper/src/components/Typography/Typography.tsx
@@ -1,7 +1,85 @@
-import type { ComponentProps } from 'react';
+import { type ComponentProps, type ElementType } from "react";
+import { cn } from "@/utils/cn";
 
-export interface TypographyProps extends ComponentProps<'p'> {}
+type DisplayVariantPermutations = {
+  variant: "display";
+  size: "lg" | "md" | "sm";
+  bold?: never;
+  mono?: never;
+};
 
-export function Typography(props: TypographyProps) {
-  return <p {...props} />;
+type HeadingVariantPermutations = {
+  variant: "heading";
+  size: "2xl" | "xl" | "lg" | "md" | "sm" | "xs";
+  bold?: never;
+  mono?: never;
+};
+
+type CopyVariantPermutations =
+  | {
+      variant: "copy";
+      size: "xl" | "lg" | "md" | "sm" | "xs";
+      mono?: never;
+      bold?: boolean;
+    }
+  | {
+      variant: "copy";
+      size: "xs";
+      mono?: true;
+      bold?: never;
+    };
+
+type LabelVariantPermutations =
+  | {
+      variant: "label";
+      size: "lg" | "md" | "sm" | "xs";
+      bold?: boolean;
+      mono?: never;
+    }
+  | {
+      variant: "label";
+      size: "md" | "sm" | "xs";
+      bold?: never;
+      mono: true;
+    };
+
+type TypographyVariantPermutations =
+  | DisplayVariantPermutations
+  | HeadingVariantPermutations
+  | CopyVariantPermutations
+  | LabelVariantPermutations;
+
+export type TypographyProps<E extends ElementType = "p"> =
+  TypographyVariantPermutations & {
+    as?: E;
+    className?: string;
+  } & Omit<ComponentProps<E>, "as" | "className">;
+
+export function Typography<E extends ElementType = "p">(
+  props: TypographyProps<E>,
+) {
+  const {
+    as: Component = "p",
+    className,
+    size,
+    variant,
+    bold,
+    mono,
+    ...rest
+  } = props;
+
+  const permutation = {
+    variant,
+    size,
+    mono,
+    bold,
+  } as TypographyVariantPermutations;
+
+  let permutationClassName = `vesper-typography-${permutation.variant}-${permutation.size}`;
+  if (permutation.bold) permutationClassName += "-bold";
+  if (permutation.mono) permutationClassName += "-mono";
+
+  return (
+    <Component className={cn(permutationClassName, className)} {...rest} />
+  );
 }
