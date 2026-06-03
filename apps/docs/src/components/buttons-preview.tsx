87f67b299a0632cc021b6b27296af53455a4db53
Simon Steer <simonpsteer@gmail.com>
2026-06-03 16:29:21 -0400

    adds text-button component

--- apps/docs/src/components/buttons-preview.tsx
+++ apps/docs/src/components/buttons-preview.tsx
@@ -2,9 +2,10 @@
 
 import { ArrowUp, Tenstorrent } from "@repo/vesper/icons";
 import { Button, type ButtonProps } from "@repo/vesper/button";
+import { TextButton, type TextButtonProps } from "@repo/vesper/text-button";
 import { IconButton } from "@repo/vesper/icon-button";
 
-const GROUPS: ButtonProps["variant"][] = [
+const BUTTON_VARIANTS: ButtonProps["variant"][] = [
   "contrast",
   "danger",
   "disabled",
@@ -15,23 +16,61 @@ const GROUPS: ButtonProps["variant"][] = [
   "warning",
 ];
 
-const SIZES: ButtonProps["size"][] = ["lg", "md", "sm", "xs"];
+const BUTTON_SIZES: ButtonProps["size"][] = ["lg", "md", "sm", "xs"];
+
+const TEXT_BUTTON_VARIANTS: TextButtonProps["variant"][] = [
+  "accent",
+  "contrast",
+  "danger",
+  "disabled",
+  "info",
+  "pink",
+  "purple",
+  "subtle",
+  "success",
+  "warning",
+];
+
+const TEXT_BUTTON_SIZES: TextButtonProps["size"][] = ["lg", "md", "sm"];
 
 export function ButtonsPreview() {
   return (
-    <div className="bg-vesper-stone-50 text-vesper-stone-900 flex flex-wrap gap-vesper-4 p-vesper-4">
-      {GROUPS.map((variant) => (
-        <div key={variant} className="flex flex-col gap-vesper-4 items-start">
-          {SIZES.map((size) => (
-            <div key={size} className="flex gap-vesper-4">
-              <Button size={size} variant={variant} iconLeft={<Tenstorrent />}>
-                {variant}
-              </Button>
-              <IconButton size={size} variant={variant} icon={<ArrowUp />} />
-            </div>
-          ))}
-        </div>
-      ))}
+    <div className="bg-vesper-stone-50 text-vesper-stone-900 flex flex-col gap-vesper-4 p-vesper-4">
+      <div className="flex flex-wrap gap-vesper-4">
+        {BUTTON_VARIANTS.map((variant) => (
+          <div key={variant} className="flex flex-col gap-vesper-4 items-start">
+            {BUTTON_SIZES.map((size) => (
+              <div key={size} className="flex gap-vesper-4">
+                <Button
+                  size={size}
+                  variant={variant}
+                  iconLeft={<Tenstorrent />}
+                >
+                  {variant}
+                </Button>
+                <IconButton size={size} variant={variant} icon={<ArrowUp />} />
+              </div>
+            ))}
+          </div>
+        ))}
+      </div>
+      <div className="flex flex-wrap gap-vesper-4">
+        {TEXT_BUTTON_VARIANTS.map((variant) => (
+          <div key={variant} className="flex flex-col gap-vesper-4 items-start">
+            {TEXT_BUTTON_SIZES.map((size) => (
+              <div key={size} className="flex gap-vesper-4">
+                <TextButton
+                  size={size}
+                  variant={variant}
+                  iconLeft={<Tenstorrent />}
+                >
+                  {variant}
+                </TextButton>
+              </div>
+            ))}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
