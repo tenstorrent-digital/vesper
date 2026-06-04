3c3400a860d57ebbd7051cd771d96b619c07eb1e
Simon Steer <simonpsteer@gmail.com>
2026-06-04 14:13:17 -0400

    add accordion component

--- apps/docs/src/app/page.tsx
+++ apps/docs/src/app/page.tsx
@@ -1,8 +1,10 @@
+import { AccordionPreview } from "@/components/accordion-preview";
 import { TypographyPreview } from "@/components/typography-preview";
 
 export default function Page() {
   return (
     <div>
+      <AccordionPreview />
       <TypographyPreview />
     </div>
   );
