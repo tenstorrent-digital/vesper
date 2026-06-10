93b97059a879e027e99286f238150a4590f522b8
Simon Steer <simonpsteer@gmail.com>
2026-06-10 15:02:21 -0400

    make banner-alert root element polymorphic

--- packages/vesper/src/components/banner-alert/banner-alert.tsx
+++ packages/vesper/src/components/banner-alert/banner-alert.tsx
@@ -1,5 +1,6 @@
 import type { ComponentProps, ElementType } from "react";
 import { cn } from "@/utils/cn";
+import { Polymorphic } from "@/utils/polymorphic";
 import { ErrorSolid } from "@/components/icon/error-solid";
 import { InfoSolid } from "@/components/icon/info-solid";
 import { Info } from "@/components/icon/info";
@@ -8,33 +9,42 @@ import { WarningSolid } from "@/components/icon/warning-solid";
 import { Button, ButtonProps } from "@/components/button/button";
 import { Typography } from "@/components/typography/typography";
 
-export interface BannerAlertProps<
-  E extends ElementType = "button",
-> extends ComponentProps<"div"> {
-  size: "sm" | "md";
-  variant: "info" | "success" | "warning" | "danger" | "secondary";
-  subtle?: boolean;
-  ctaAs?: E;
-  cta?: Omit<ButtonProps<E>, "size" | "variant" | "as">;
-}
+export type BannerAlertProps<
+  E extends ElementType = "div",
+  C extends ElementType = "button",
+> = Polymorphic<
+  {
+    size: "sm" | "md";
+    variant: "info" | "success" | "warning" | "danger" | "secondary";
+    subtle?: boolean;
+    ctaAs?: C;
+    cta?: Omit<ButtonProps<C>, "size" | "variant" | "as">;
+  },
+  E
+>;
 
 const BANNER_ALERT_TYPOGRAPHY_SIZES = {
   md: "sm",
   sm: "xs",
 } as const;
 
-export function BannerAlert<E extends ElementType = "button">({
-  className,
-  size,
-  variant,
-  ctaAs,
-  cta,
-  children,
-  subtle,
-  ...props
-}: BannerAlertProps<E>) {
+export function BannerAlert<
+  E extends ElementType = "div",
+  C extends ElementType = "button",
+>(props: BannerAlertProps<E, C>) {
+  const {
+    as: Component = "div",
+    className,
+    size,
+    variant,
+    ctaAs,
+    cta,
+    children,
+    subtle,
+    ...rest
+  } = props;
   return (
-    <div
+    <Component
       className={cn(
         "vesper-banner-alert",
         `vesper-banner-alert-${size}`,
@@ -42,7 +52,7 @@ export function BannerAlert<E extends ElementType = "button">({
         subtle && "vesper-banner-alert-subtle",
         className,
       )}
-      {...props}
+      {...rest}
     >
       <div className="vesper-banner-alert-content">
         <span className="vesper-banner-alert-icon">
@@ -68,6 +78,6 @@ export function BannerAlert<E extends ElementType = "button">({
           size="sm"
         />
       )}
-    </div>
+    </Component>
   );
 }
