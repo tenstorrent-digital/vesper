6b1ea8dfe07f79fbe96f631c08d00b9ae0252325
Simon Steer <simonpsteer@gmail.com>
2026-06-13 17:58:00 -0400

    update avatar-group.stories.tsx, add stories for typography

--- packages/vesper/src/components/avatar-group/avatar-group.stories.tsx
+++ packages/vesper/src/components/avatar-group/avatar-group.stories.tsx
@@ -6,7 +6,6 @@ const meta = {
   component: AvatarGroup,
   parameters: { layout: "centered" },
   argTypes: {
-    avatars: { control: "number" },
     as: { table: { disable: true } },
   },
 } satisfies Meta<typeof AvatarGroup>;
@@ -18,16 +17,9 @@ type Story = StoryObj<typeof meta>;
 export const Playground: Story = {
   args: {
     size: "md",
-    avatars: 5 as any,
-  },
-  render: ({ avatars: numAvatars = 3, ...props }) => {
-    const avatars = Array.from({ length: numAvatars as number }).map(
-      (_, index) => ({
-        src: `https://unsplash.it/${(index + 1) * 150}/${(index + 1) * 150}`,
-      }),
-    );
-
-    return <AvatarGroup avatars={avatars} {...props} />;
+    avatars: Array.from({ length: 5 }).map((_, index) => ({
+      src: `https://unsplash.it/${(index + 1) * 150}/${(index + 1) * 150}`,
+    })),
   },
 };
 
