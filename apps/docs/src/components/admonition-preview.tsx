c37a3cdf962c286998444d4d1d6ad9a7d5131d85
Simon Steer <simonpsteer@gmail.com>
2026-06-13 12:52:45 -0400

    update some previews to use constants

--- apps/docs/src/components/admonition-preview.tsx
+++ apps/docs/src/components/admonition-preview.tsx
@@ -1,27 +1,21 @@
 "use client";
 
-import { Admonition, type AdmonitionProps } from "@repo/vesper/admonition";
-
-const VARIANTS: AdmonitionProps["variant"][] = [
-  "danger",
-  "info",
-  "secondary",
-  "success",
-  "warning",
-];
-
-const SIZES: AdmonitionProps["size"][] = ["sm", "md"];
+import {
+  Admonition,
+  ADMONITION_VARIANTS,
+  ADMONITION_SIZES,
+} from "@repo/vesper/admonition";
 
 export function AdmonitionPreview() {
   return (
     <div className="bg-vesper-stone-50 text-vesper-stone-900 flex flex-col gap-vesper-4 p-vesper-4">
       <div className="flex flex-wrap gap-vesper-4">
-        {VARIANTS.map((variant) => (
+        {ADMONITION_VARIANTS.map((variant) => (
           <div
             key={variant}
             className="flex-1 flex flex-col gap-vesper-4 items-start"
           >
-            {SIZES.map((size) => (
+            {ADMONITION_SIZES.map((size) => (
               <div key={size} className="flex flex-col gap-vesper-4 w-full">
                 <Admonition size={size} variant={variant}>
                   {variant}
