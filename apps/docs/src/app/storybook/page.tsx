c831b097a6942de1d83ec6c89fa4dc7d56d51a7b
Simon Steer <simonpsteer@gmail.com>
2026-06-17 14:29:14 -0400

    Add an accessible title to iframe

--- apps/docs/src/app/storybook/page.tsx
+++ apps/docs/src/app/storybook/page.tsx
@@ -1,7 +1,11 @@
 export default function Page() {
   return (
     <div className="fixed inset-0">
-      <iframe className="w-full h-full" src="/storybook/index.html" />
+      <iframe
+        title="Vesper Storybook"
+        className="w-full h-full"
+        src="/storybook/index.html"
+      />
     </div>
   );
 }
