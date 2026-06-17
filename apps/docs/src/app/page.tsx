bee47a3000d24d6f85f008542b45b6c84b5b5cec
Simon Steer <simonpsteer@gmail.com>
2026-06-17 14:59:56 -0400

    render storybook at the root of the application

--- apps/docs/src/app/page.tsx
+++ apps/docs/src/app/page.tsx
@@ -1,27 +1,11 @@
-import { BadgePreview } from "@/components/badge-preview";
-import { AccordionPreview } from "@/components/accordion-preview";
-import { AvatarPreview } from "@/components/avatar-preview";
-import { ProgressBarPreview } from "@/components/progress-bar-preview";
-import { ButtonsPreview } from "@/components/buttons-preview";
-import { MenuPreview } from "@/components/menu-preview";
-import { TypographyPreview } from "@/components/typography-preview";
-import { AdmonitionPreview } from "@/components/admonition-preview";
-import { TooltipPreview } from "@/components/tooltip-preview";
-import { ThemeSwitcherPreview } from "@/components/theme-switcher-preview";
-
 export default function Page() {
   return (
-    <div>
-      <TypographyPreview />
-      <ThemeSwitcherPreview />
-      <TooltipPreview />
-      <AdmonitionPreview />
-      <BadgePreview />
-      <AccordionPreview />
-      <AvatarPreview />
-      <ProgressBarPreview />
-      <MenuPreview />
-      <ButtonsPreview />
+    <div className="fixed inset-0">
+      <iframe
+        title="Vesper Storybook"
+        className="w-full h-full"
+        src="/storybook/index.html"
+      />
     </div>
   );
 }
