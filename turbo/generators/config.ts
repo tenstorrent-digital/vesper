f78b99ca2d7384f77bf3d645e852131a2cbb1914
Simon Steer <simonpsteer@gmail.com>
2026-06-17 11:43:28 -0400

    Update config.ts

--- turbo/generators/config.ts
+++ turbo/generators/config.ts
@@ -33,6 +33,7 @@ export default function generator(plop: PlopTypes.NodePlopAPI): void {
       {
         type: "add",
         path: "{{ turbo.paths.root }}/packages/vesper/src/components/{{ kebabCase name }}/{{ kebabCase name }}.css",
+        template: ".vesper-{{ kebabeCase name }} {\n}",
       },
       {
         type: "add",
