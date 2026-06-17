b0be9183cea25feed1aaba9d6149b790b78c82f7
Simon Steer <simonpsteer@gmail.com>
2026-06-17 17:02:21 -0400

    redirect from homepage of docs to storybook temporarily

--- apps/docs/src/app/page.tsx
+++ apps/docs/src/app/page.tsx
@@ -1,27 +1,5 @@
-import { BadgePreview } from "@/components/badge-preview";
-import { AccordionPreview } from "@/components/accordion-preview";
-import { AvatarPreview } from "@/components/avatar-preview";
-import { ProgressBarPreview } from "@/components/progress-bar-preview";
-import { ButtonsPreview } from "@/components/buttons-preview";
-import { MenuPreview } from "@/components/menu-preview";
-import { TypographyPreview } from "@/components/typography-preview";
-import { AdmonitionPreview } from "@/components/admonition-preview";
-import { TooltipPreview } from "@/components/tooltip-preview";
-import { ThemeSwitcherPreview } from "@/components/theme-switcher-preview";
+import { redirect } from "next/navigation";
 
 export default function Page() {
-  return (
-    <div>
-      <TypographyPreview />
-      <ThemeSwitcherPreview />
-      <TooltipPreview />
-      <AdmonitionPreview />
-      <BadgePreview />
-      <AccordionPreview />
-      <AvatarPreview />
-      <ProgressBarPreview />
-      <MenuPreview />
-      <ButtonsPreview />
-    </div>
-  );
+  redirect("/storybook");
 }
