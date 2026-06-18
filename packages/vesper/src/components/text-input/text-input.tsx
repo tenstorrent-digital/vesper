ca6d1ff3db03d1156fe1ff565521ba9aff7b84ba
Simon Steer <simonpsteer@gmail.com>
2026-06-18 15:54:57 -0400

    create prop type for text-input

--- packages/vesper/src/components/text-input/text-input.tsx
+++ packages/vesper/src/components/text-input/text-input.tsx
@@ -1,8 +1,92 @@
-import type { ComponentProps } from 'react';
-import { cn } from '@/utils/cn';
+import type { ComponentProps, ReactNode } from "react";
+import { cn } from "@/utils/cn";
 
-export interface TextInputProps extends ComponentProps<'input'> {}
+export const TEXT_INPUT_SIZES = ["sm", "md", "lg"] as const;
 
-export function TextInput({ className, ...props }: TextInputProps) {
-  return <input className={cn("vesper-text-input", className)} {...props} />;
+export type TextInputSize = (typeof TEXT_INPUT_SIZES)[number];
+
+type InputPropTypes =
+  | "onFocus"
+  | "onFocusCapture"
+  | "onBlur"
+  | "onBlurCapture"
+  | "onChange"
+  | "onChangeCapture"
+  | "onBeforeInput"
+  | "onBeforeInputCapture"
+  | "onInput"
+  | "onInputCapture"
+  | "onReset"
+  | "onResetCapture"
+  | "onSubmit"
+  | "onSubmitCapture"
+  | "onInvalid"
+  | "onInvalidCapture"
+  | "onKeyDown"
+  | "onKeyDownCapture"
+  | "onKeyUp"
+  | "onKeyUpCapture";
+
+export interface TextInputProps
+  extends
+    Omit<ComponentProps<"div">, InputPropTypes>,
+    Pick<ComponentProps<"input">, InputPropTypes> {
+  size?: TextInputSize;
+  icon?: ReactNode;
+  type?: "text" | "email" | "password" | "url";
+}
+
+export function TextInput({
+  className,
+  icon,
+  type,
+  size,
+  onFocus,
+  onFocusCapture,
+  onBlur,
+  onBlurCapture,
+  onChange,
+  onChangeCapture,
+  onBeforeInput,
+  onBeforeInputCapture,
+  onInput,
+  onInputCapture,
+  onReset,
+  onResetCapture,
+  onSubmit,
+  onSubmitCapture,
+  onInvalid,
+  onInvalidCapture,
+  onKeyDown,
+  onKeyDownCapture,
+  onKeyUp,
+  onKeyUpCapture,
+  ...props
+}: TextInputProps) {
+  return (
+    <div className={cn("vesper-text-input", className)} {...props}>
+      <input
+        onFocus={onFocus}
+        onFocusCapture={onFocusCapture}
+        onBlur={onBlur}
+        onBlurCapture={onBlurCapture}
+        onChange={onChange}
+        onChangeCapture={onChangeCapture}
+        onBeforeInput={onBeforeInput}
+        onBeforeInputCapture={onBeforeInputCapture}
+        onInput={onInput}
+        onInputCapture={onInputCapture}
+        onReset={onReset}
+        onResetCapture={onResetCapture}
+        onSubmit={onSubmit}
+        onSubmitCapture={onSubmitCapture}
+        onInvalid={onInvalid}
+        onInvalidCapture={onInvalidCapture}
+        onKeyDown={onKeyDown}
+        onKeyDownCapture={onKeyDownCapture}
+        onKeyUp={onKeyUp}
+        onKeyUpCapture={onKeyUpCapture}
+      />
+    </div>
+  );
 }
