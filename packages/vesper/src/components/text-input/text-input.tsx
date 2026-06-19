f96d698d126814f8e33f21e7eba906e4a9151048
Simon Steer <simonpsteer@gmail.com>
2026-06-19 10:50:41 -0400

    update text-input prop types

--- packages/vesper/src/components/text-input/text-input.tsx
+++ packages/vesper/src/components/text-input/text-input.tsx
@@ -26,7 +26,7 @@ export type TextInputSize = (typeof TEXT_INPUT_SIZES)[number];
 export type TextInputVariant = (typeof TEXT_INPUT_VARIANTS)[number];
 
 /**
- * Union of all the prop types that should be forwarded to the input element, and excluded from the containing div element
+ * Union of all the prop types that should be forwarded to the input/textarea element, and excluded from the containing div element
  * */
 type ForwardedPropTypes =
   | "defaultValue"
@@ -90,7 +90,7 @@ export type MultiLineTextInputProps = Omit<
 
 export type SingleLineTextInputProps = Omit<
   ComponentProps<"div">,
-  ForwardedPropTypes | InputOnlyPropTypes
+  ForwardedPropTypes
 > &
   Pick<ComponentProps<"input">, ForwardedPropTypes | InputOnlyPropTypes> & {
     multiline?: false;
@@ -100,7 +100,7 @@ export type SingleLineTextInputProps = Omit<
     message?: string;
     label?: string;
     icon?: ReactNode;
-    type?: "text" | "email" | "password" | "url";
+    type?: "text" | "email" | "password" | "url" | "tel" | "search";
     height?: never;
   };
 
