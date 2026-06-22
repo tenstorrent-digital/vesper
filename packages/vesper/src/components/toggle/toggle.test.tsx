2df5bdbfe989b8e517a5d56082a35c73811fb5e9
Simon Steer <simonpsteer@gmail.com>
2026-06-22 13:27:46 -0400

    add snapshot and a11y tests

--- packages/vesper/src/components/toggle/toggle.test.tsx
+++ packages/vesper/src/components/toggle/toggle.test.tsx
@@ -3,22 +3,56 @@ import { beforeEach, afterEach, describe, expect, test } from "vitest";
 import axe from "axe-core";
 
 import { Toggle } from "@/components/toggle/toggle";
+import { Globe, Tenstorrent } from "@/components/icons/icons";
 
 import "@/styles/test.css";
 
 afterEach(cleanup);
 
-describe("toggle [unit]", () => {
-  test("renders null", () => {
-    const { container } = render(<Toggle />);
-    expect(container.firstChild).toBeNull();
-  });
-});
+describe("toggle [unit]", () => {});
 
 describe("toggle [snapshot]", () => {
-  test("renders correctly", async () => {
-    const { container } = render(<Toggle />);
+  test("text options", async () => {
+    const { container } = render(
+      <Toggle
+        options={[
+          { text: "Option A", value: "option-a", ariaLabel: "aria-label A" },
+          { text: "Option B", value: "option-b", ariaLabel: "aria-label B" },
+        ]}
+      />,
+    );
+    expect(container.firstChild).toMatchSnapshot();
+  });
 
+  test("icon options", async () => {
+    const { container } = render(
+      <Toggle
+        options={[
+          { icon: <Globe />, value: "option-a", ariaLabel: "aria-label A" },
+          {
+            icon: <Tenstorrent />,
+            value: "option-b",
+            ariaLabel: "aria-label B",
+          },
+        ]}
+      />,
+    );
+    expect(container.firstChild).toMatchSnapshot();
+  });
+
+  test("mixed options", async () => {
+    const { container } = render(
+      <Toggle
+        options={[
+          { text: "Option A", value: "option-a", ariaLabel: "aria-label A" },
+          {
+            icon: <Tenstorrent />,
+            value: "option-b",
+            ariaLabel: "aria-label B",
+          },
+        ]}
+      />,
+    );
     expect(container.firstChild).toMatchSnapshot();
   });
 });
@@ -33,8 +67,34 @@ describe("toggle [a11y]", () => {
       document.documentElement.removeAttribute("data-vesper-theme");
     });
 
-    test(`wcag2aaa (${theme})`, async () => {
-      const { container } = render(<Toggle />);
+    test(`wcag2aaa (text, ${theme})`, async () => {
+      const { container } = render(
+        <Toggle
+          options={[
+            { text: "Option A", value: "option-a", ariaLabel: "aria-label A" },
+            { text: "Option B", value: "option-b", ariaLabel: "aria-label B" },
+          ]}
+        />,
+      );
+
+      expect(
+        await axe.run(container, { runOnly: "wcag2aaa" }),
+      ).toHaveNoViolations();
+    });
+
+    test(`wcag2aaa (icons, ${theme})`, async () => {
+      const { container } = render(
+        <Toggle
+          options={[
+            { icon: <Globe />, value: "option-a", ariaLabel: "aria-label A" },
+            {
+              icon: <Tenstorrent />,
+              value: "option-b",
+              ariaLabel: "aria-label B",
+            },
+          ]}
+        />,
+      );
 
       expect(
         await axe.run(container, { runOnly: "wcag2aaa" }),
