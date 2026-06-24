9f150594dd97c5218a48e35153c859cc0bde93af
Simon Steer <simonpsteer@gmail.com>
2026-06-24 10:53:52 -0400

    add support for rendering label next to switch

--- packages/vesper/src/components/switch/switch.stories.tsx
+++ packages/vesper/src/components/switch/switch.stories.tsx
@@ -5,6 +5,9 @@ import { Switch } from "@/components/switch/switch";
 const meta = {
   component: Switch,
   parameters: { layout: "centered" },
+  argTypes: {
+    label: { control: "text" },
+  },
 } satisfies Meta<typeof Switch>;
 
 export default meta;
@@ -14,6 +17,7 @@ type Story = StoryObj<typeof meta>;
 export const Playground: Story = {
   args: {
     size: "md",
+    label: "Label",
   },
 };
 Playground.storyName = "switch";
--- packages/vesper/src/components/switch/switch.tsx
+++ packages/vesper/src/components/switch/switch.tsx
@@ -1,9 +1,14 @@
+import { ReactNode } from "react";
 import {
   type SwitchProps as RadixSwitchProps,
   Switch as RadixSwitch,
   SwitchThumb,
 } from "@radix-ui/react-switch";
 import { cn } from "@/utils/cn";
+import {
+  Typography,
+  type TypographyVariant,
+} from "@/components/typography/typography";
 
 export const SWITCH_SIZES = ["sm", "md"] as const;
 
@@ -11,10 +16,21 @@ export type SwitchSize = (typeof SWITCH_SIZES)[number];
 
 export interface SwitchProps extends Omit<RadixSwitchProps, "asChild"> {
   size?: SwitchSize;
+  label?: ReactNode;
 }
 
-export function Switch({ size = "md", className, ...props }: SwitchProps) {
-  return (
+const SWITCH_TYPOGRAPHY: { [S in SwitchSize]: TypographyVariant } = {
+  sm: "label-md",
+  md: "label-lg",
+};
+
+export function Switch({
+  size = "md",
+  label,
+  className,
+  ...props
+}: SwitchProps) {
+  const switchEl = (
     <RadixSwitch
       className={cn("vesper-switch", `vesper-switch-${size}`, className)}
       {...props}
@@ -22,4 +38,15 @@ export function Switch({ size = "md", className, ...props }: SwitchProps) {
       <SwitchThumb className="vesper-switch-thumb" />
     </RadixSwitch>
   );
+
+  if (label) {
+    return (
+      <label className="vesper-switch-label">
+        {switchEl}
+        <Typography variant={SWITCH_TYPOGRAPHY[size]}>{label}</Typography>
+      </label>
+    );
+  }
+
+  return switchEl;
 }
