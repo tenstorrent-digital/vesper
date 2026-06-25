b9d34981d6fbfc92a8983294a40e56615b1b65d2
Simon Steer <simonpsteer@gmail.com>
2026-06-25 15:37:32 -0400

    flesh out props & DOM structure of tag component

--- packages/vesper/src/components/tag/tag.tsx
+++ packages/vesper/src/components/tag/tag.tsx
@@ -1,8 +1,65 @@
-import type { ComponentProps } from 'react';
-import { cn } from '@/utils/cn';
+import type { ElementType, ReactNode } from "react";
+import type { Polymorphic } from "@/utils/polymorphic";
+import { cn } from "@/utils/cn";
 
-export interface TagProps extends ComponentProps<'div'> {}
+export const TAG_SIZES = ["sm", "md", "lg"] as const;
 
-export function Tag({ className, ...props }: TagProps) {
-  return <div className={cn("vesper-tag", className)} {...props} />;
+export const TAG_VARIANTS = [
+  "default",
+  "contrast",
+  "accent-bold",
+  "accent-subtle",
+  "danger-bold",
+  "danger-subtle",
+  "success-bold",
+  "success-subtle",
+  "info-bold",
+  "info-subtle",
+  "warning-bold",
+  "warning-subtle",
+] as const;
+
+export type TagSize = (typeof TAG_SIZES)[number];
+
+export type TagVariant = (typeof TAG_VARIANTS)[number];
+
+export type TagProps<E extends ElementType = "div"> = Polymorphic<
+  {
+    size?: TagSize;
+    variant?: TagVariant;
+    disabled?: boolean;
+    icon?: ReactNode;
+  },
+  E
+>;
+
+export function Tag<E extends ElementType = "div">(props: TagProps<E>) {
+  const {
+    as: Component = "div",
+    className,
+    size = "lg",
+    variant = "default",
+    disabled,
+    icon,
+    children,
+    ...rest
+  } = props;
+
+  return (
+    <Component
+      disabled={disabled}
+      aria-disabled={disabled}
+      className={cn(
+        "vesper-tag",
+        `vesper-tag-${size}`,
+        `vesper-tag-${variant}`,
+        disabled && "vesper-tag-disabled",
+        className,
+      )}
+      {...rest}
+    >
+      {icon && <span className="vesper-tag-icon">{icon}</span>}
+      {children}
+    </Component>
+  );
 }
