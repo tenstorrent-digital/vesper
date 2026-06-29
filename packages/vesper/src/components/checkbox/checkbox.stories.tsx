22ceea73e87bcb440e004b6cea3852982fae43a9
Simon Steer <simonpsteer@gmail.com>
2026-06-29 09:04:14 -0400

    checkbox first styling pass

--- packages/vesper/src/components/checkbox/checkbox.stories.tsx
+++ packages/vesper/src/components/checkbox/checkbox.stories.tsx
@@ -5,6 +5,9 @@ import { Checkbox } from "@/components/checkbox/checkbox";
 const meta = {
   component: Checkbox,
   parameters: { layout: "centered" },
+  argTypes: {
+    defaultChecked: { table: { disable: true } },
+  },
 } satisfies Meta<typeof Checkbox>;
 
 export default meta;
@@ -12,6 +15,12 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Playground: Story = {
-  args: {},
+  args: {
+    size: "md",
+    label: "Label",
+    disabled: false,
+    defaultChecked: "indeterminate",
+    required: false,
+  },
 };
 Playground.storyName = "checkbox";
--- packages/vesper/src/components/checkbox/checkbox.tsx
+++ packages/vesper/src/components/checkbox/checkbox.tsx
@@ -1,5 +1,68 @@
-export interface CheckboxProps {}
+import type { ReactNode } from "react";
+import {
+  Checkbox as RadixCheckbox,
+  CheckboxIndicator,
+  type CheckboxProps as RadixCheckboxProps,
+} from "@radix-ui/react-checkbox";
+import { cn } from "@/utils/cn";
+import {
+  Typography,
+  type TypographyVariant,
+} from "@/components/typography/typography";
+import { Checkmark, Minus } from "@/components/icons/icons";
 
-export function Checkbox(props: CheckboxProps) {
-  return null;
+export const CHECKBOX_SIZES = ["sm", "md"] as const;
+
+export type CheckboxSize = (typeof CHECKBOX_SIZES)[number];
+
+export interface CheckboxProps extends Omit<
+  RadixCheckboxProps,
+  "asChild" | "children"
+> {
+  label: string;
+  size?: CheckboxSize;
+}
+
+const CHECKBOX_TYPOGRAPHY: { [S in CheckboxSize]: TypographyVariant } = {
+  sm: "label-md",
+  md: "label-lg",
+};
+
+export function Checkbox({
+  required,
+  label,
+  size = "md",
+  className,
+  disabled,
+  ...props
+}: CheckboxProps) {
+  return (
+    <label
+      className={cn(
+        "vesper-checkbox",
+        `vesper-checkbox-${size}`,
+        disabled && "vesper-checkbox-disabled",
+        className,
+      )}
+    >
+      <RadixCheckbox
+        className="vesper-checkbox-box"
+        required={required}
+        disabled={disabled}
+        {...props}
+      >
+        <CheckboxIndicator className="vesper-checkbox-indicator">
+          <Checkmark className="vesper-checkbox-checked-icon" />
+          <Minus className="vesper-checkbox-indeterminate-icon" />
+        </CheckboxIndicator>
+      </RadixCheckbox>
+      <Typography
+        variant={CHECKBOX_TYPOGRAPHY[size]}
+        className="vesper-checkbox-label"
+        as="span"
+      >
+        {required ? label + " *" : label}
+      </Typography>
+    </label>
+  );
 }
