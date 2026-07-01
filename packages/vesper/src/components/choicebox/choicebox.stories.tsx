bea5229b1b3ca8cc189d8ec8ec28bf3471cc0f57
Simon Steer <simonpsteer@gmail.com>
2026-07-01 15:42:47 -0400

    flesh out single select styling

--- packages/vesper/src/components/choicebox/choicebox.stories.tsx
+++ packages/vesper/src/components/choicebox/choicebox.stories.tsx
@@ -12,6 +12,30 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Playground: Story = {
-  args: {},
+  args: {
+    name: "example",
+    options: [
+      {
+        label: "Option A",
+        value: "option-a",
+        description: "This is a description",
+      },
+      {
+        label: "Option B",
+        value: "option-b",
+        description: "This is a description",
+      },
+      {
+        label: "Option C",
+        value: "option-c",
+        description: "This is a description",
+        disabled: true,
+      },
+    ],
+    multiselect: false,
+    disabled: false,
+    defaultValue: "option-c",
+    style: { width: "min(calc(100vw - 4rem), 400px)" },
+  },
 };
 Playground.storyName = "choicebox";
--- packages/vesper/src/components/choicebox/choicebox.tsx
+++ packages/vesper/src/components/choicebox/choicebox.tsx
@@ -1,8 +1,126 @@
-import type { ComponentProps } from 'react';
-import { cn } from '@/utils/cn';
+import type { ComponentProps } from "react";
+import { cn } from "@/utils/cn";
+import { Typography } from "@/components/typography/typography";
 
-export interface ChoiceboxProps extends ComponentProps<'fieldset'> {}
+export type ChoiceboxItem = {
+  value: string;
+  label: string;
+  description?: string;
+  disabled?: boolean;
+  id?: string;
+};
 
-export function Choicebox({ className, ...props }: ChoiceboxProps) {
-  return <fieldset className={cn("vesper-choicebox", className)} {...props} />;
+interface ChoiceboxBaseProps extends Omit<
+  ComponentProps<"fieldset">,
+  "children" | "onChange" | "defaultValue"
+> {
+  name: string;
+  options: ChoiceboxItem[];
+  disabled?: boolean;
+}
+
+export interface ChoiceboxSingleSelectProps extends ChoiceboxBaseProps {
+  multiselect?: false;
+  required?: boolean;
+  value?: string;
+  defaultValue?: string;
+  onChange?(value: string): void;
+}
+
+export interface ChoiceboxMultiSelectProps extends ChoiceboxBaseProps {
+  multiselect: true;
+  value?: string[];
+  defaultValue?: string[];
+  onChange?(values: string[]): void;
+}
+
+export type ChoiceboxProps =
+  | ChoiceboxSingleSelectProps
+  | ChoiceboxMultiSelectProps;
+
+export function Choicebox(props: ChoiceboxProps) {
+  if (props.multiselect) return <ChoiceboxMultiSelect {...props} />;
+  return <ChoiceboxSingleSelect {...props} />;
+}
+
+function ChoiceboxSingleSelect({
+  className,
+  value,
+  defaultValue,
+  onChange,
+  options,
+  name,
+  disabled,
+  required,
+  ...props
+}: ChoiceboxSingleSelectProps) {
+  return (
+    <fieldset className={cn("vesper-choicebox", className)} {...props}>
+      {options.map((option) => {
+        const isDisabled = option.disabled || disabled;
+
+        const checkedProps =
+          value !== undefined
+            ? { checked: value === option.value }
+            : { defaultChecked: defaultValue === option.value };
+
+        return (
+          <label
+            key={option.value}
+            htmlFor={option.id}
+            className={cn(
+              "vesper-choicebox-item",
+              !option.description && "vesper-choicebox-item-compact",
+            )}
+          >
+            <div>
+              <Typography
+                variant="copy-sm-bold"
+                className="vesper-choicebox-item-label"
+              >
+                {option.label}
+              </Typography>
+              {option.description && (
+                <Typography
+                  variant="copy-xs"
+                  className="vesper-choicebox-item-description"
+                >
+                  {option.description}
+                </Typography>
+              )}
+            </div>
+            <input
+              id={option.id}
+              name={name}
+              required={required}
+              disabled={isDisabled}
+              type="radio"
+              className="vesper-choicebox-radio-input"
+              onChange={(e) => {
+                if (!onChange) return;
+                if (e.target.checked) onChange(option.value);
+              }}
+              {...checkedProps}
+            />
+            <div className="vesper-choicebox-radio-input-indicator" />
+          </label>
+        );
+      })}
+    </fieldset>
+  );
+}
+
+function ChoiceboxMultiSelect({
+  className,
+  value,
+  defaultValue,
+  onChange,
+  ...props
+}: ChoiceboxMultiSelectProps) {
+  return (
+    <fieldset
+      className={cn("vesper-choicebox", className)}
+      {...props}
+    ></fieldset>
+  );
 }
