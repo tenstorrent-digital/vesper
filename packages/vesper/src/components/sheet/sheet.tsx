7a908a3246b83a52fa741f78a4ffb997da2c8d1c
Simon Steer <simonpsteer@gmail.com>
2026-07-13 13:55:13 -0500

    add title and description props to SheetProps interface

--- packages/vesper/src/components/sheet/sheet.tsx
+++ packages/vesper/src/components/sheet/sheet.tsx
@@ -1,7 +1,10 @@
-import type { ComponentProps } from 'react';
-import { cn } from '@/utils/cn';
+import type { ComponentProps } from "react";
+import { cn } from "@/utils/cn";
 
-export interface SheetProps extends ComponentProps<'dialog'> {}
+export interface SheetProps extends Omit<ComponentProps<"dialog">, "open"> {
+  title: string;
+  description: string;
+}
 
 export function Sheet({ className, ...props }: SheetProps) {
   return <dialog className={cn("vesper-sheet", className)} {...props} />;
