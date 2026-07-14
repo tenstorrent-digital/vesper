d5e184e33a21435b7825ff2bcf7c0987fb1a6716
Simon Steer <simonpsteer@gmail.com>
2026-07-14 13:29:26 -0500

    Update sheet.stories.tsx

--- packages/vesper/src/components/sheet/sheet.stories.tsx
+++ packages/vesper/src/components/sheet/sheet.stories.tsx
@@ -1,11 +1,55 @@
 import type { Meta, StoryObj } from "@storybook/react-vite";
 
-import { Sheet, useSheet } from "@/components/sheet/sheet";
+import {
+  Sheet,
+  SHEET_SIDES,
+  type SheetProps,
+  useSheet,
+} from "@/components/sheet/sheet";
 import { Button } from "@/components/button/button";
+import { Typography } from "@/components/typography/typography";
+
+function SheetStoryComponent({
+  withButtons,
+  ...props
+}: SheetProps & { withButtons: boolean }) {
+  const sheet = useSheet();
+
+  return (
+    <>
+      <Button onClick={sheet.open} variant="contrast">
+        Open sheet
+      </Button>
+      <Sheet
+        ref={sheet.ref}
+        {...props}
+        buttons={
+          withButtons
+            ? [
+                { children: "Close", onClick: sheet.close },
+                { children: "Next", onClick: sheet.close },
+              ]
+            : []
+        }
+      >
+        <Typography>
+          {`Don't be afraid to make decisions. Put your feelings into it, your heart, it's your world. You are just a whisper floating across a mountain.`}
+        </Typography>
+      </Sheet>
+    </>
+  );
+}
 
 const meta = {
-  component: Sheet,
-} satisfies Meta<typeof Sheet>;
+  component: SheetStoryComponent,
+  argTypes: {
+    side: { control: "radio", options: SHEET_SIDES },
+    withButtons: {
+      description:
+        "Not a `Toast` component prop; this control is used to easily toggle on/off buttons in the storybook preview UI.",
+    },
+  },
+} satisfies Meta<typeof SheetStoryComponent>;
 
 export default meta;
 
@@ -16,18 +60,7 @@ export const Playground: Story = {
     title: "Sheet title",
     description: "Sheet description",
     side: "right",
-  },
-  render: function Render(props) {
-    const sheet = useSheet();
-
-    return (
-      <>
-        <Button onClick={sheet.open} variant="contrast">
-          Open sheet
-        </Button>
-        <Sheet ref={sheet.ref} {...props} />
-      </>
-    );
+    withButtons: false,
   },
 };
 Playground.storyName = "sheet";
