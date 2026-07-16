a485523f461412dfdd3573a7e3d1d68d5ac31267
Simon Steer <simonpsteer@gmail.com>
2026-07-16 11:13:29 -0400

    add JSDoc comments to `setupCodeBlock` function

--- packages/vesper/src/components/code-block/store.ts
+++ packages/vesper/src/components/code-block/store.ts
@@ -19,13 +19,53 @@ class CodeBlockStore {
     highlighter: null,
   };
 
-  setupCodeBlock = async ({ langs }: { langs: LanguageInput[] }) => {
+  /**
+   * Setup function that must be called before rendering a `CodeBlock`. This function should only be called once in your application.
+   * */
+  setupCodeBlock = async ({
+    langs,
+    aliases,
+  }: {
+    /**
+     * Array of language grammars to support. You can install the `@shikijs/langs` package and pick and choose which languages to support by importing the individual grammars. To support plaintext only, you can pass an empty array.
+     *
+     * @example
+     * ```ts
+     * await registerLanguages({
+     *   langs: [
+     *     import("@shikijs/langs/typescript"),
+     *     import("@shikijs/langs/css"),
+     *     import("@shikijs/langs/json"),
+     *     import("@shikijs/langs/markdown"),
+     *     import("@shikijs/langs/shellscript"),
+     *   ],
+     * });
+     * ```
+     */
+    langs: LanguageInput[];
+    /**
+     * A record of aliases for the languages you wish to support. This field is optional.
+     *
+     * @example
+     * ```ts
+     * {
+     *   js: 'javascript',
+     *   ts: 'typescript',
+     *   sh: 'shellscript',
+     *   py: 'python',
+     *   rs: 'rust',
+     * }
+     * ```
+     * */
+    aliases?: { [key: string]: string };
+  }) => {
     this.state = {
       initialized: true,
       highlighter: await createHighlighterCore({
         langs,
         engine: createJavaScriptRegexEngine(),
         themes: [theme],
+        langAlias: aliases,
       }),
     };
   };
