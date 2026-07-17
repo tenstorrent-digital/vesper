1eb8da4b57bb3164c9e7e7dba43ec79274eee0eb
Simon Steer <simonpsteer@gmail.com>
2026-07-17 15:22:55 -0400

    add tokens for using dots patterm backgrounds

---
+++ packages/vesper/src/tokens/backgrounds.ts
@@ -0,0 +1,10 @@
+export const backgrounds = {
+  ["dots-primary"]:
+    "var(--vesper-pattern-dots) repeat top var(--vesper-stone-0)",
+  ["dots-secondary"]:
+    "var(--vesper-pattern-dots) repeat top var(--vesper-stone-50)",
+  ["dots-tertiary"]:
+    "var(--vesper-pattern-dots) repeat top var(--vesper-stone-100)",
+  ["dots-inverted"]:
+    "var(--vesper-pattern-dots-inverted) repeat top var(--vesper-stone-900)",
+};
--- packages/vesper/src/tokens/index.ts
+++ packages/vesper/src/tokens/index.ts
@@ -1,5 +1,7 @@
+export * from "./backgrounds";
 export * from "./colors";
 export * from "./leading";
+export * from "./patterns";
 export * from "./radius";
 export * from "./shadows";
 export * from "./spacing";
