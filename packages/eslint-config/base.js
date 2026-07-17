688ad564cf48558bf5a1c57987ac258559f107da
Mackenzie Davidson <mwvdavidson@gmail.com>
2026-07-17 07:27:13 -0400

    add import sorting to base eslint config

--- packages/eslint-config/base.js
+++ packages/eslint-config/base.js
@@ -3,6 +3,7 @@ import eslintConfigPrettier from "eslint-config-prettier";
 import turboPlugin from "eslint-plugin-turbo";
 import tseslint from "typescript-eslint";
 import onlyWarn from "eslint-plugin-only-warn";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
 
 /**
  * A shared ESLint configuration for the repository.
@@ -24,6 +25,46 @@ export const config = [
   {
     plugins: {
       onlyWarn,
+    },
+  },
+  {
+    plugins: {
+      "simple-import-sort": simpleImportSort,
+    },
+    rules: {
+      "simple-import-sort/imports": [
+        "warn",
+        {
+          groups: [
+            // packages
+            // `react` related packages come first
+            ["^react", "^@?\\w"],
+
+            // internal packages from monorepo
+            ["^(@repo)(/.*|$)"],
+
+            // imports from named paths in tsconfig.json (in order)
+            ["^(@/components)(/.*|$)"],
+            ["^(@/lib)(/.*|$)"],
+            ["^(@/utils)(/.*|$)"],
+            // then any other named path
+            ["^(@/\\w)(/.*|$)"],
+
+            // side effect
+            ["^\\u0000"],
+
+            // parent imports, `..` last
+            ["^\\.\\.(?!/?$)", "^\\.\\./?$"],
+            // other relative imports
+            // put same-folder imports and `.` last
+            ["^\\./(?=.*/)(?!/?$)", "^\\.(?!/?$)", "^\\./?$"],
+
+            // css
+            ["^.+\\.s?css$"],
+          ],
+        },
+      ],
+      "simple-import-sort/exports": "warn",
     },
   },
   {
