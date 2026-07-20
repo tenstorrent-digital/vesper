15bbd11d491b9e5c05cd4bd264f3e438051677f4
Mackenzie Davidson <mwvdavidson@gmail.com>
2026-07-20 15:08:07 -0400

    add type definitions for frontmatter, update remark plugins to be compatible with `turborepo`, add example, update doc comments

--- apps/docs/next.config.ts
+++ apps/docs/next.config.ts
@@ -1,14 +1,12 @@
 import createMDX from "@next/mdx";
 import type { NextConfig } from "next";
-import remarkFrontmatter from "remark-frontmatter";
-import remarkGfm from "remark-gfm";
-import remarkMdxFrontmatter from "remark-mdx-frontmatter";
 
 const nextConfig: NextConfig = {
   pageExtensions: [
     "ts",
     "tsx",
-    // allow creating pages with markdown
+
+    // allow creating next `page.{ext}`s with markdown
     "md",
     "mdx",
   ],
@@ -21,7 +19,24 @@ const withMDX = createMDX({
   extension: /\.(md|mdx)$/,
 
   options: {
-    remarkPlugins: [remarkGfm, remarkFrontmatter, remarkMdxFrontmatter],
+    /**
+     * to use remark/rehype plugins with turborepo, we have to
+     * specify plugin names using a string (no need for `import`)
+     *
+     * @see https://nextjs.org/docs/app/guides/mdx#using-plugins-with-turbopack
+     */
+    remarkPlugins: [
+      "remark-gfm",
+
+      /**
+       * add support for accessing frontmatter from md/mdx
+       *
+       * @see type definitions: apps/docs/src/types/mdx.d.ts
+       * @see (extremely light) documentation: https://nextjs.org/docs/app/guides/mdx#frontmatter
+       */
+      "remark-frontmatter",
+      "remark-mdx-frontmatter",
+    ],
   },
 });
 
