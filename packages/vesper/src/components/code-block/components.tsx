0f919f08f1ffb5108db7cdd8283d98313a952f79
Simon Steer <simonpsteer@gmail.com>
2026-07-22 15:22:41 -0400

    show success state when copy to clipboard is successful

--- packages/vesper/src/components/code-block/components.tsx
+++ packages/vesper/src/components/code-block/components.tsx
@@ -1,9 +1,9 @@
 "use client";
 
-import type { ComponentProps } from "react";
+import { type ComponentProps, useEffect, useState } from "react";
 
 import { IconButton } from "@/components/icon-button/icon-button";
-import { Copy } from "@/components/icons/icons";
+import { Checkmark, Copy } from "@/components/icons/icons";
 import { Typography } from "@/components/typography/typography";
 
 export function CodeBlockPreWrapper({
@@ -23,10 +23,17 @@ export function CodeBlockPreWrapper({
 }
 
 export function CopyToClipboardButton() {
+  const [copied, setCopied] = useState(false);
+  useEffect(() => {
+    if (!copied) return;
+    const id = setTimeout(() => setCopied(false), 2000);
+    return () => clearTimeout(id);
+  }, [copied]);
+
   return (
     <IconButton
       variant="tertiary"
-      icon={<Copy />}
+      icon={copied ? <Checkmark /> : <Copy />}
       aria-label="Copy code"
       size="sm"
       type="button"
@@ -40,7 +47,10 @@ export function CopyToClipboardButton() {
          * If a consumer supplies transformers to a code block instance, the passed in code string may contain hidden comments that tell the highlighter to output modified hast nodes. In such cases we want to copy the rendered text content because it will omit those hidden comments.
          */
         const content = pre?.textContent || "";
-        navigator.clipboard?.writeText(content).catch(() => {});
+        navigator.clipboard
+          ?.writeText(content)
+          .then(() => setCopied(true))
+          .catch(() => {});
       }}
     />
   );
