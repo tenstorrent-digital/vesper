3f4fbc9ac00189f768d8254ea19b0b70e74c1222
Simon Steer <simonpsteer@gmail.com>
2026-07-23 13:44:35 -0400

    Update shadows.ts

--- packages/vesper/src/tokens/shadows.ts
+++ packages/vesper/src/tokens/shadows.ts
@@ -4,6 +4,6 @@ export const shadows = {
   ["300"]: "0 0.75rem 2rem 0 rgba(9, 34, 33, 0.15)",
   ["400"]: "0 1.5rem 4rem 0 rgba(9, 34, 33, 0.2)",
   ["inset-100"]: "0 0.125rem 0.375rem 0 var(--vesper-alpha-contrast-100) inset",
-  ["inset-200"]: "0 0.125rem 0.75rem 0 var(--tint-contrast-200) inset",
+  ["inset-200"]: "0 0.125rem 0.75rem 0 var(--vesper-tint-contrast-200) inset",
   ["focus-ring"]: "0 0 0 0.1875rem var(--vesper-teal-200)",
 };
