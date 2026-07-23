637ca931f9e28d90a46fb6044344d93652a73fd3
Mackenzie Davidson <mwvdavidson@gmail.com>
2026-07-23 17:51:24 -0400

    filter components page.tsx from sidebar

--- apps/docs/src/components/nav/sidebar/index.tsx
+++ apps/docs/src/components/nav/sidebar/index.tsx
@@ -3,7 +3,9 @@ import fs from "node:fs";
 import { Tree } from "./tree";
 
 // get all folders in /components
-const componentPages = fs.readdirSync("src/app/components");
+const componentPages = fs
+  .readdirSync("src/app/components")
+  .filter((page) => !page.endsWith(".tsx"));
 
 export const Sidebar = () => {
   return (
