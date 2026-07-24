b19e15297dac55f883e4edf4ad2ff09f20c72ae6
Simon Steer <simonpsteer@gmail.com>
2026-07-24 14:58:17 -0400

    Add JSDoc comments to exported Snippet component function

--- packages/vesper/src/components/snippet/snippet.tsx
+++ packages/vesper/src/components/snippet/snippet.tsx
@@ -13,10 +13,27 @@ export type SnippetVariant = (typeof SNIPPET_VARIANTS)[number];
 export interface SnippetProps extends Omit<ComponentProps<"div">, "children"> {
   /** The code text to display in the snippet. Also used as the value copied to the clipboard when the copy button is clicked. */
   children?: string;
-  /** The visual style variant of the snippet. Defaults to `"default"`. */
+  /** The visual style variant of the snippet. @default fault */
   variant?: SnippetVariant;
 }
 
+/**
+ * A copyable code snippet component that displays monospace text with a built-in copy-to-clipboard button.
+ *
+ * @param {string} [props.children] - (optional) The code text to display and copy to clipboard
+ * @param {SnippetVariant} [props.variant] - (optional) The visual style variant of the snippet. @default default`
+ * @param {string} [props.className] - (optional) Additional CSS class names to apply
+ *
+ * You may also pass any additional props to the underlying `div` element
+ *
+ * @example
+ * <Snippet>yarn add some-package</Snippet>
+ *
+ * @example
+ * <Snippet variant="contrast">
+ *   {`export default function App() {\n  return <div />\n}`}
+ * </Snippet>
+ */
 export function Snippet(props: SnippetProps) {
   const { className, children = "", variant = "default", ...rest } = props;
 
