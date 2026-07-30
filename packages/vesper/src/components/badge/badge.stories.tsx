d134e63bc585ef885608b7342f15a4759867b297
Simon Steer <simonpsteer@gmail.com>
2026-07-30 09:36:03 -0400

    update badge default size to "md"

--- packages/vesper/src/components/badge/badge.stories.tsx
+++ packages/vesper/src/components/badge/badge.stories.tsx
@@ -18,7 +18,7 @@ type Story = StoryObj<typeof meta>;
 export const Playground: Story = {
   args: {
     variant: "accent",
-    size: "lg",
+    size: "md",
     children: "Label",
     subtle: false,
     icon: false,
