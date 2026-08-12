4d0e8bc64a46d50fee386269fc1b465b41bd2b58
Simon Steer <simonpsteer@gmail.com>
2026-08-12 17:54:08 -0400

    update toast documentation

--- apps/website/src/demos/toast.tsx
+++ apps/website/src/demos/toast.tsx
@@ -1,19 +1,44 @@
 "use client";
 
-import { Button } from "@tenstorrent/vesper/button";
-import { addToast, Toasts } from "@tenstorrent/vesper/toast";
+import type { ReactNode } from "react";
 
-export function ToastDemo() {
-  return (
-    <>
+import { Button } from "@tenstorrent/vesper/button";
+import { addToast, type ToastOptions, Toasts } from "@tenstorrent/vesper/toast";
+
+type ToastDemoProps =
+  | {
+      kind: "container";
+    }
+  | { kind: "spawner"; options: ToastOptions; children?: ReactNode }
+  | {
+      kind: "async-spawner";
+      options: [ToastOptions, ToastOptions];
+      children?: ReactNode;
+      timeout: number;
+    };
+
+export function ToastDemo(props: ToastDemoProps) {
+  if (props.kind === "container") {
+    return <Toasts />;
+  }
+
+  if (props.kind === "spawner") {
+    return (
+      <Button onClick={() => addToast(props.options)}>{props.children}</Button>
+    );
+  }
+
+  if (props.kind === "async-spawner") {
+    return (
       <Button
-        onClick={() =>
-          addToast({ content: "Hello from Vesper!", timeout: 5000 })
-        }
+        onClick={() => {
+          const [optionsA, optionsB] = props.options;
+          const toast = addToast(optionsA);
+          setTimeout(() => toast.update(optionsB), props.timeout);
+        }}
       >
-        Show Toast
+        {props.children}
       </Button>
-      <Toasts />
-    </>
-  );
+    );
+  }
 }
