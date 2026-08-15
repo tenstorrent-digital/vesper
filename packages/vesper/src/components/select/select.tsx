55566f215226051062187955ff4f0b04490e6b49
Simon Steer <simonpsteer@gmail.com>
2026-08-14 21:43:54 -0400

    update select.tsx

--- packages/vesper/src/components/select/select.tsx
+++ packages/vesper/src/components/select/select.tsx
@@ -3,10 +3,7 @@
 import {
   type ComponentProps,
   type ReactNode,
-  useCallback,
-  useEffect,
   useImperativeHandle,
-  useRef,
   useState,
 } from "react";
 import { Select as BaseSelect } from "@base-ui/react/select";
@@ -180,11 +177,11 @@ export function Select(props: SelectProps) {
                   value={o.value}
                   className="vesper-select-item"
                 >
-                  <Typography variant="label-md">
-                    <BaseSelect.ItemText render={<span />}>
-                      {o.label}
-                    </BaseSelect.ItemText>
-                  </Typography>
+                  <BaseSelect.ItemText
+                    render={<Typography variant="label-md" />}
+                  >
+                    {o.label}
+                  </BaseSelect.ItemText>
                   <BaseSelect.ItemIndicator className="vesper-select-item-checkmark">
                     <Checkmark />
                   </BaseSelect.ItemIndicator>
