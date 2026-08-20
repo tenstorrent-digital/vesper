94df92b35da2119d44cfcdc396d2e85f6091bb00
Simon Steer <simonpsteer@gmail.com>
2026-08-20 10:02:09 -0400

    update IconsDemo to show tooltip with icon kind on hover

--- apps/website/src/demos/icons.tsx
+++ apps/website/src/demos/icons.tsx
@@ -1,10 +1,13 @@
 import { Icon, ICON_KINDS } from "@tenstorrent/vesper/icons";
+import { Tooltip } from "@tenstorrent/vesper/tooltip";
 
 export function IconsDemo() {
   return (
     <div className="flex flex-wrap gap-1">
       {ICON_KINDS.map((kind) => (
-        <Icon key={kind} kind={kind} style={{ width: 24 }} />
+        <Tooltip key={kind} content={kind}>
+          <Icon kind={kind} style={{ width: 24 }} />
+        </Tooltip>
       ))}
     </div>
   );
