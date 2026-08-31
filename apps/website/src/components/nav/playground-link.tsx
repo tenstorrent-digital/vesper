380f9c88ae8b123428bcf7f2612eb799c360b523
Mackenzie Davidson <mwvdavidson@gmail.com>
2026-08-31 11:51:05 -0400

    let playground button navigate to component story if on component doc

--- apps/website/src/components/nav/playground-link.tsx
+++ apps/website/src/components/nav/playground-link.tsx
@@ -1,17 +1,31 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+
 import { Button } from "@tenstorrent/vesper/button";
 
 export const PlaygroundLink = () => {
+  const pathname = usePathname();
+  const pathSegments = pathname.split("/");
+
+  let storybookUrl =
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:5173"
+      : "/storybook";
+
+  const isComponentPage =
+    pathname.startsWith("/components/") &&
+    // get the component name from the pathname (first segment is empty
+    // because pathnames start with `/`)
+    pathSegments.length === 3;
+
+  if (isComponentPage) {
+    const componentName = pathSegments[2];
+    storybookUrl += `?path=/story/components-${componentName}--playground`;
+  }
+
   return (
-    <Button
-      as="a"
-      href={
-        process.env.NODE_ENV === "development"
-          ? "http://localhost:5173"
-          : "/storybook"
-      }
-      variant="tertiary"
-      size="sm"
-    >
+    <Button as="a" href={storybookUrl.toString()} variant="tertiary" size="sm">
       Playground
     </Button>
   );
