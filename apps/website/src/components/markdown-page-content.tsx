2a25cc59155e621f0dbb12a219c74d71ffd6c9e4
Simon Steer <simonpsteer@gmail.com>
2026-09-15 15:08:20 -0400

    toc styling tweaks

--- apps/website/src/components/markdown-page-content.tsx
+++ apps/website/src/components/markdown-page-content.tsx
@@ -1,8 +1,10 @@
-import { ReactNode } from "react";
+import type { ReactNode } from "react";
+import Link from "next/link";
 
-import { ToCItem } from "@/lib/filesystem/docs/types";
+import { Typography } from "@tenstorrent/vesper/typography";
 
-import { ToC } from "./toc";
+import type { ToCItem } from "@/lib/filesystem/docs/types";
+import { cn } from "@/lib/tailwind/cn";
 
 export function MarkdownPageContent({
   toc,
@@ -14,7 +16,31 @@ export function MarkdownPageContent({
   return (
     <div className="flex items-start justify-between">
       <main className="prose min-w-0">{children}</main>
-      <ToC toc={toc} />
+      <aside
+        className={cn(
+          "sticky top-(--nav-scroll-margin)",
+          "gap-vesper-1 hidden flex-col xl:flex",
+          "pl-vesper-4 w-3xs shrink-0"
+        )}
+      >
+        <Typography variant="label-lg-bold" className="mb-vesper-4">
+          On this page
+        </Typography>
+        {toc.map((item) => (
+          <Typography
+            key={item.id}
+            as={Link}
+            variant="copy-xs"
+            href={`#${item.id}`}
+            className="text-vesper-text-tertiary hover:underline"
+            style={{
+              marginLeft: `calc(var(--vesper-spacing-3) * ${item.depth - 1})`,
+            }}
+          >
+            {item.text}
+          </Typography>
+        ))}
+      </aside>
     </div>
   );
 }
