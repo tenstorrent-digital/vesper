98850fe258b5e0dacee2376082cc28ca035149f1
Simon Steer <simonpsteer@gmail.com>
2026-09-16 16:38:29 -0400

    use admonition for `not-found`

--- apps/website/src/app/not-found.tsx
+++ apps/website/src/app/not-found.tsx
@@ -1,21 +1,11 @@
-import Link from "next/link";
-
-import { Button } from "@tenstorrent/vesper/button";
-import { ArrowLeft } from "@tenstorrent/vesper/icons";
-import { Typography } from "@tenstorrent/vesper/typography";
+import { Admonition } from "@tenstorrent/vesper/admonition";
 
 export default function NotFound() {
   return (
     <main>
-      <Typography variant="heading-2xl" as="h1">
-        Not Found
-      </Typography>
-      <Typography className="mt-vesper-8 mb-vesper-4">
+      <Admonition variant="danger">
         Could not find requested resource
-      </Typography>
-      <Button iconLeft={<ArrowLeft />} as={Link} href="/" variant="subtle">
-        Back to docs
-      </Button>
+      </Admonition>
     </main>
   );
 }
