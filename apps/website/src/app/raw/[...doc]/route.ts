f98b08b9a2456408e7e00b867e01e6044e92a3cb
Simon Steer <simonpsteer@gmail.com>
2026-09-16 16:25:32 -0400

    Add comment about pointing to sitemap in raw doc response

--- apps/website/src/app/raw/[...doc]/route.ts
+++ apps/website/src/app/raw/[...doc]/route.ts
@@ -12,6 +12,7 @@ export function generateStaticParams() {
 export async function GET(_: Request, context: RouteContext<"/raw/[...doc]">) {
   const { doc } = await context.params;
 
+  // We should point to our sitemap in the 404 response here when it is available
   const entry = getDoc(doc);
   if (!entry) return new Response("Not found", { status: 404 });
 
