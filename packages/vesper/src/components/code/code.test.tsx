01551aa6345527d35869e27a4ae1c8dd3cbf35b7
Simon Steer <simonpsteer@gmail.com>
2026-09-18 14:05:23 -0400

    Update code.test.tsx

--- packages/vesper/src/components/code/code.test.tsx
+++ packages/vesper/src/components/code/code.test.tsx
@@ -77,11 +77,13 @@ describe("code [a11y]", () => {
   describe.each(["light", "dark"] as const)("theme: %s", (theme) => {
     beforeEach(() => {
       document.documentElement.setAttribute("data-vesper-theme", theme);
+      document.body.style.setProperty("color", "var(--vesper-text-primary)");
       document.body.style.setProperty("background", "var(--vesper-stone-0)");
     });
 
     afterEach(() => {
       document.documentElement.removeAttribute("data-vesper-theme");
+      document.body.style.removeProperty("color");
       document.body.style.removeProperty("background");
     });
 
